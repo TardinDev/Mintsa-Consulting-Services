e9335207f2e01a73526be75512b845ac659a8f89
TardinDev <tardindavy@gmail.com>
2024-11-23 17:36:40 +0100

    ⚡️ HomePage > refactoring all code

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,21 @@
+
+
+import Catalogue from './Catalogue/Catalogue'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Hero from './Header/Hero'
+
+export default function Homepage() {
+
+
+  return (
+
+   <>
+     <Header />
+     <Hero />
+     <Catalogue />
+     <Footer />
+   </>
+  
+  )
+}
