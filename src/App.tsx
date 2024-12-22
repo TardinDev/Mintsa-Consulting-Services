0cfb0944c39a932e53651d69c3b60f8308790da6
TardinDev <tardindavy@gmail.com>
2024-12-22 18:12:50 +0100

    🐛 App > fix a bug about dataProduct

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './components/Homepage/Homepage';
+import LoginPage from './components/LoginPage/LoginPage';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './utils/Route/ProtectedRoute';
+import Admin from './components/Admin/Admin';
 
-import Homepage from './components/Homepage/Homepage'
 
-function App() {
-
-  return (
+const App: React.FC = () => {
   
-     <Homepage/>    
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute >
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </AuthProvider>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
