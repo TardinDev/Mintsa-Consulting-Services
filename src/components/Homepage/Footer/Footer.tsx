a491590a5100c90cf02d486ace4156d54725dc3c
TardinDev <tardindavy@gmail.com>
2025-01-03 07:58:22 +0100

    🎨 Footer > refactoring

--- src/components/Homepage/Footer/Footer.tsx
+++ src/components/Homepage/Footer/Footer.tsx
@@ -1,46 +1,33 @@
-import React from 'react';
 import styled from 'styled-components';
 import theme from '../../../utils/Theme/theme';
+import { services, agreements, recognitions } from '../../../utils/data/footerData';
+import { FaAddressCard, FaFacebook, FaInstagram, FaLinkedin, FaMap, FaPhone, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa';
 import { FaMessage } from 'react-icons/fa6';
-import { 
-       FaAddressCard, FaFacebook, FaInstagram, FaLinkedin, FaMap, 
-       FaPhone, FaPinterest, FaTwitter, FaYoutube 
-} from 'react-icons/fa';
+import FooterSection from './FooterSection';
+import SocialIcons from './SocialIcons';
 
 
-const services = [
-  "Consulting", "Design", "Development", "E-business", 
-  "E-commerce", "E-marketing", "Hosting", "SEO", "Social Media"
-];
+// Définir le type SocialLinkType
+type SocialLinkType = {
+  icon: React.ReactElement; // Utilisez React.ReactElement pour le type de l'icône
+  href: string;
+  aria: string;
+};
 
-const agreements = [
-  "Business & Retail", "E-commerce & Shopping", 
-  "Education & Organisations", "News & Entertainment", 
-  "Industrial & Manufacturing", "Social Media", 
-  "Sports & Leisure", "Travel & Tourism"
-];
-
-const recognitions = [
-  "International CONSULTING ASSOCIATION", 
-  "GABON WEB BUSINESS", 
-  "CHAMBRE DE COMMERCE Companies LIBREVILLE", 
-  "AGENCE NATIONALE DE LA CONSOMMATION ET DE LA CONCURRENCE"
-];
-
-const socialLinks = [
+// Définir le tableau socialLinks
+const socialLinks: SocialLinkType[] = [
   { icon: <FaTwitter size={25} color="blue" />, href: "#", aria: "Twitter" },
   { icon: <FaFacebook size={25} color="white" />, href: "https://www.facebook.com/cedriclebonheur/?locale=fr_FR", aria: "Facebook" },
   { icon: <FaLinkedin size={25} color="lightblue" />, href: "#", aria: "LinkedIn" },
   { icon: <FaInstagram size={25} color="darkred" />, href: "#", aria: "Instagram" },
   { icon: <FaPinterest size={25} color="white" />, href: "#", aria: "Pinterest" },
-  { icon: <FaYoutube size={25} color="red" />, href: "#", aria: "YouTube" }
+  { icon: <FaYoutube size={25} color="red" />, href: "#", aria: "YouTube" },
 ];
 
+
 const Footer: React.FC = () => (
-  
   <FooterContainer>
-    <FooterSection>
-      <h3>SERVICES</h3>
+    <FooterSection title="SERVICES">
       <ul>
         {services.map((service, index) => (
           <li key={index}>{service}</li>
@@ -48,8 +35,7 @@ const Footer: React.FC = () => (
       </ul>
     </FooterSection>
 
-    <FooterSection>
-      <h3>AGREMENTS</h3>
+    <FooterSection title="AGREMENTS">
       <ul>
         {agreements.map((agreement, index) => (
           <li key={index}>{agreement}</li>
@@ -57,8 +43,7 @@ const Footer: React.FC = () => (
       </ul>
     </FooterSection>
 
-    <FooterSection>
-      <h3>CONTACT US</h3>
+    <FooterSection title="CONTACT US">
       <p><FaPhone size={20} color="lightgrey" /> +241 74 85 34 84</p>
       <p>
         <FaMessage size={20} color="lightgrey" />
@@ -68,15 +53,8 @@ const Footer: React.FC = () => (
       <p><FaMap size={20} color="lightgrey" /> <a href="#">Voir sur la carte</a></p>
     </FooterSection>
 
-    <FooterSection>
-      <h3>RESTER CONNECTÉ</h3>
-      <SocialIcons>
-        {socialLinks.map((link, index) => (
-          <a key={index} href={link.href} aria-label={link.aria}>
-            {link.icon}
-          </a>
-        ))}
-      </SocialIcons>
+    <FooterSection title="RESTER CONNECTÉ">
+      <SocialIcons links={socialLinks} />
       <h3>RECONNU PAR</h3>
       <ul>
         {recognitions.map((item, index) => (
@@ -102,49 +80,6 @@ const FooterContainer = styled.footer`
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: center;
-  }
-`;
-
-const FooterSection = styled.div`
-  margin: 20px;
-  flex: 1;
-  min-width: 200px;
-
-  h3 {
-    color: ${theme.blue};
-    margin-bottom: 10px;
-  }
-
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-
-  ul li {
-    margin: 8px 0;
-  }
-
-  p, a {
-    color: ${theme.white};
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-`;
-
-const SocialIcons = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-top: 10px;
-
-  a {
-    font-size: 1.2em;
-    transition: color 0.3s;
-  }
-
-  a:hover {
-    color: #0056b3;
   }
 `;
 
